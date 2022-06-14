584aa50c78361aa39f8838aab32c9a458b139255
Njeta Melvin Ndi <njetami@mail.uc.edu>
2022-06-13 21:28:59 -0400

    update

--- src/ShipCostCalculato.java
+++ src/ShipCostCalculato.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+
 public class ShipCostCalculato {
     public static void main(String[] args) {
+        double TAX_RATE = 0.0;
+        double itemCost = 200;
+        double shippingCost;
+        double totalPrice;
 
+//       if (itemCost < 100);
+        {
+
+//          shippingCost = itemCost * TAX_RATE;
+//          totalPrice = itemCost + shippingCost;
+
+//            System.out.println("The shipping cost is " + itemCost + " " + TAX_RATE + " is " + shippingCost);
+//            System.out.println("The total price of the item is " + itemCost + " " + shippingCost + " is " + totalPrice);
+        }
+
+        if (itemCost >= 100) ;
+        {
+            shippingCost = itemCost * TAX_RATE;
+            totalPrice = itemCost + shippingCost;
+            System.out.println("The shipping cost is " + itemCost + " " + TAX_RATE + " is " + shippingCost);
+            System.out.println("The total price of the item is " + itemCost + " " + shippingCost + " is " + totalPrice);
+        }
     }
 }
